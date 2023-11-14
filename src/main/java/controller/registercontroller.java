a554f0ff27db2cb4791376a13d702198d7192e24
Sochetra <chetras@users.noreply.github.com>
2023-11-14 23:39:11 +0700

    commit 2 tra

--- src/main/java/controller/registercontroller.java
+++ src/main/java/controller/registercontroller.java
@@ -1,4 +1,16 @@
 package controller;
 
+import com.store.logininterface.main;
+import javafx.event.Event;
+
+import java.io.IOException;
+
 public class registercontroller {
+
+    public void handleloginbutton2(Event e) throws IOException {
+        main me = new main();
+        me.changeScene("login-view.fxml");
+    }
 }
+
+
