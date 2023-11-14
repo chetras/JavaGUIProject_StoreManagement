1499b568ae93c64ea412f04f7bc36a2897806c61
Sochetra <chetras@users.noreply.github.com>
2023-11-14 22:44:27 +0700

    commit 1 (Tra)

--- src/main/java/com/store/logininterface/main.java
+++ src/main/java/com/store/logininterface/main.java
@@ -2,19 +2,28 @@ package com.store.logininterface;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class main extends Application {
+    private static Stage stg;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("helllo-view.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        stg = primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException{
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
--- src/main/java/controller/logincontroller.java
+++ src/main/java/controller/logincontroller.java
@@ -1,13 +1,18 @@
 package controller;
 
+import com.store.logininterface.main;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class logincontroller {
+    @FXML
+    public Label wronglogin;
+
     @FXML
     public TextField usernamefield;
 
@@ -16,25 +21,25 @@ public class logincontroller {
 
 
     public void handleloginbutton (Event e) throws IOException {
-        String username = usernamefield.getText();
-        String password = passwordfield.getText();
-
-        if(isValidInput(username, password)){
-
-
-        }
+        checkLogin();
     }
 
-    private boolean isValidInput(String username, String password) {
-        if ( username == null || username.isEmpty() || password == null || password.isEmpty()){
-            HelperMethod.alertbox("Please enter the correct Email and Password",null,"Login failed" );
-            return false;
+    public void checkLogin() throws IOException{
+        main m = new main();
+        if(usernamefield.getText().toString().equals("John") && passwordfield.getText().toString().equals("123")){
+            wronglogin.setText("Success");
+        }
+        else if (usernamefield.getText().isEmpty() && passwordfield.getText().isEmpty()){
+            wronglogin.setText("Please enter user data.");
         }
         else {
-            return true;
+            wronglogin.setText("Wrong username or password");
         }
     }
 
+    public void handleregisterbutton(Event e) throws IOException{
+        main me = new main();
+        me.changeScene("register-view.fxml");
 
-
+    }
 }
